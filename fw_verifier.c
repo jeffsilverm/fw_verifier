e3a98a6422559210a22d236e959c7effe8893c46
Jeff Silverman <jeffsilverm@gmail.com>
2022-01-04 21:13:26 -0800

    Multiprocessing scheme works as planned.  Except I have to use multithreaded

---
+++ fw_verifier.c
@@ -0,0 +1,64 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <errno.h>
+#include <unistd.h>
+
+#define MAX_CHILDREN    20
+
+int annotate(int status, char *generation){
+    if ( status == 0 ){
+        printf("In a child: my PID is %d  and PPID is %d.  Genertion is %s\n",
+            getpid(), getppid(), generation );
+        sleep(60.0);
+        printf("Child process %d is exiting and waiting for the parent to reap.\n", getpid() );
+        fflush(stdout);
+        exit(0);
+        }
+    else if ( status > 0 ) {
+        printf("Just forked %s child PID %d, my PID is %d  and PPID is %d\n", generation, status, getpid(), getppid() );
+        fflush(stdout);
+    }
+    else {
+        printf("Something awful happened!  status is %d\n", status );
+        fflush(stdout);
+        exit(status);
+    };
+}
+    
+
+int main()
+{
+    int status;
+    char annotation[1000];
+    int i=1;
+    while ( i< MAX_CHILDREN ) {
+      i++;
+      sprintf ( annotation, "%u\n", i);
+      status = fork();
+      annotate( status, annotation );
+      printf("I have now forked %d children\n", i);
+      if ( i > MAX_CHILDREN * 2 ) {
+          printf("I have forked too many children %d now I am going to break\n", i);
+          fflush(stdout);
+          break;
+      } else {
+          fflush(stdout);
+      }
+    };
+    printf("Now reaping children\n");
+    while ( 1 ) {
+        pid_t wstatus;
+        printf("Waiting for any child to finish\n");
+        wstatus = waitpid(-1, 0, 0);
+        if ( wstatus < 0 && errno == ECHILD ) {
+            printf("Done reaping children\n");
+            break;
+        } else {
+            printf("Child %d has finished\n", wstatus);
+        };
+
+    }
+    return 0;
+}
